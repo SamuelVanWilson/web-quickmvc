4946ed783b3d7aa2f5c98efe16c82af0a0a48b4b
SamuelVan <samuelvanmartino@gmail.com>
2024-10-15 11:55:26 +0700

    [layout] component: add component alert

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import Layout from "./components/Layout.js";
+import Alert from "./components/Alert.js";
 document.addEventListener('DOMContentLoaded', function () {
     customElements.define('header-component', Header)
     customElements.define('footer-component', Footer)
+    customElements.define('alert-component', Alert)
     customElements.define('layout-component', Layout)
 })
